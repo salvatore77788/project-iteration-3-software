7c2f7804918a09325f822e5f9fe92c1df56a9bdd
Vince <101427142+salvatore77788@users.noreply.github.com>
2022-04-02 18:24:54 -0600

    Delete SCS - Hardware - v3.0.1/SCS - Hardware - v3.0 directory

--- SCS - Hardware - v3.0.1/SCS - Hardware - v3.0/src/org/lsmr/selfcheckout/devices/Emitter.java
+++
@@ -1,9 +0,0 @@
-package org.lsmr.selfcheckout.devices;
-
-/**
- * A simple base interface for devices that emit things.
- * 
- * @param <T>
- *            The type of the things to emit.
- */
-public interface Emitter<T> {}
