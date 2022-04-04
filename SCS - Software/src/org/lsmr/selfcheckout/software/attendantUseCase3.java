f56ae3a55cedeba7af818c54076e43a4795d03ea
Rainethhh <Rainethcubed@gmail.com>
2022-04-04 13:49:39 -0600

    empty methods added

--- SCS - Software/src/org/lsmr/selfcheckout/software/attendantUseCase3.java
+++ SCS - Software/src/org/lsmr/selfcheckout/software/attendantUseCase3.java
@@ -1,5 +1,19 @@
 package org.lsmr.selfcheckout.software;
 
 public class attendantUseCase3 {
-
+	public void startup() {
+		
+	}
+	
+	public void shutdown() {
+		
+	}
+	
+	public void login() {
+		
+	}
+	
+	public void logout() {
+		
+	}
 }
