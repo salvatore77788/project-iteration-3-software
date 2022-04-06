549e92186b06885a52472fd56e92fc71ec5fe8cb
guagy <guagy@10.0.0.64>
2022-04-06 13:07:41 -0600

    password database

--- SCS - Software/src/org/lsmr/selfcheckout/software/passwordDatabase.java
+++ SCS - Software/src/org/lsmr/selfcheckout/software/passwordDatabase.java
@@ -1,5 +1,22 @@
 package org.lsmr.selfcheckout.software;
 
-public class passwordDatabase {
+import java.util.HashMap;
 
+public class PasswordDatabase {
+	private HashMap<int, String> database = new HashMap<int, String>();
+	
+	public PasswordDatabase(HashMap<int, String> map) {
+		this.database = map;
+	}
+	
+	public BarcodedProductDatabase(){
+	}
+	
+	public void AddLoginDetails(int ID, String password){
+		database.put(ID, password);
+	}
+	
+	public Password getPassword(int ID) {
+		return database.get(ID);
+	}
 }
