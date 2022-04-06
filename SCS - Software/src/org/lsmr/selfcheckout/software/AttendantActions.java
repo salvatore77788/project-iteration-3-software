343ec4f4fb6a69179141b4a9605cfbd0fe64deaa
JordanTTran <JordanTTran@hotmail.com>
2022-04-06 15:53:56 -0600

    Add attendant removes item
    add removeItem method to remove item from customer checkout machine.

--- SCS - Software/src/org/lsmr/selfcheckout/software/AttendantActions.java
+++ SCS - Software/src/org/lsmr/selfcheckout/software/AttendantActions.java
@@ -1,4 +1,4 @@
-package org.lsmr.software;
+package org.lsmr.selfcheckout.software;
 
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
 
@@ -28,4 +28,63 @@ public class AttendantActions {
         data.setIsDisabled(false);
     }
 
+    // Remove scanned item from customer station
+    public void removeItem(SelfCheckoutStation station) throws Exception {
+    	
+    	SelfCheckoutStationSoftware stationSoftware = new SelfCheckoutStationSoftware(station);
+    	ElectronicScaleSoftware scaleSoftware = new ElectronicScaleSoftware(station);
+    	
+    	
+    	int count = 0;
+    	
+    	// Display all items scanned by customer
+    	for(ItemInfo item: stationSoftware.itemsScanned) {
+    		System.out.println("Item count: " + count + " Item Description: "+ item.description + " Item Price: $" + item.price);
+    		count++;
+    	}
+    	
+		Scanner scanner = new Scanner(System.in);
+
+		boolean valid = false;
+		int itemNum = 0;
+		
+		// Choose item to remove
+		// Should replace with GUI
+		while(!valid) {
+			
+			// Get input and check if it is valid 
+			try {
+				System.out.print("Enter Item Number to Remove: ");
+				
+				// Get input from attendant
+				String itemString = scanner.nextLine();
+				
+				// Try to convert to int
+				itemNum = Integer.parseInt(itemString);
+				
+				// If int selected is greater then the number of items the customer has scanned then it is invalid
+				if(itemNum >= stationSoftware.itemsScanned.size()) {
+					throw new Exception();
+				}
+				
+				System.out.println("Item #" + itemNum + " was selected");
+				valid = true;
+			} catch (Exception e) {
+				System.out.println("Invalid selection, please try again");
+			}
+		}
+    	
+    	// Get item to remove
+    	ItemInfo item = stationSoftware.itemsScanned.get(itemNum);
+    	
+    	// Mark item removed on ElectronicScaleSoftware so the scale works properly once the item is taken off bagging area
+    	scaleSoftware.setAttendantRemovedItem();
+    	
+    	// Remove item
+    	stationSoftware.itemsScanned.remove(item);
+    	
+    	
+    }
+    
+    
 }
