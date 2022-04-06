296f9435ff35d7ab491addc05bf40a39e6a77c4c
doowoncho <84430881+doowoncho@users.noreply.github.com>
2022-04-06 13:06:42 -0600

    login logout

--- SCS - Software/src/org/lsmr/selfcheckout/software/attendantUseCase3.java
+++ SCS - Software/src/org/lsmr/selfcheckout/software/attendantUseCase3.java
@@ -1,9 +1,13 @@
 package org.lsmr.selfcheckout.software;
 
-import org.lsmr.selfcheckout.devices.Keyboard;
+import java.util.List;
+
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.devices.SupervisionStation;
 
 public class attendantUseCase3 {
 	
+	public boolean isLoggedIn = false;
 	//Potentially add methods to add or remove supervised stations if no other class has done that already
 	
 	public void startup() {
@@ -16,19 +20,37 @@ public class attendantUseCase3 {
 		//can only be done while logged in
 	}
 	
-	public void login() {
+	
+	//id and password inputted for a given superVisionStation
+	public void login(int id, String password, SupervisionStation superStation) {
 		
-		KeyBoardObserver.keyPressed();
-		//
-		//
+		if(database.method(id, password) == true && isLoggedIn == false)
+		{
+				
+			    superStation.screen.enable();
+			    superStation.keyboard.enable();
+			    isLoggedIn = true;
+	
+		}
 		
 		//Have attendant enter an employee ID and password, and test if the entries occur in a password database of some sort
 		//On successful login, enable functions/objects related to supervision stations. 
 		//Can only be run properly if current station is not already logged into. 
 	}
 	
-	public void logout() {
-		//reverse of login, doesn't require password, just have the attendant press a logout button, and disable corresponding functions.
-		//can only be done if already logged in.
+	public void logout(SupervisionStation superStation) {
+		if(isLoggedIn == true)
+		{
+			 superStation.screen.disable();
+			 superStation.keyboard.disable();
+		}
 	}
+	
+	// Getter for the log in status
+	  public Boolean getLoginStatus() {
+	    return isLoggedIn;
+	  }
+
+	
+	
 }
