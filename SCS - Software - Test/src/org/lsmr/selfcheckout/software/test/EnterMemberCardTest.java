62dbf205b617a13b6cfc6f7efb6f34d7bd1368aa
Carlos Veintimilla <veintimilla.carlos@outlook.com>
2022-04-09 16:18:23 -0600

    Testing GUI for "Enter member card number"
    Test happens on Frame from SelfCheckOutStation.
    The keypad has yet to be tested.

--- SCS - Software - Test/src/org/lsmr/selfcheckout/software/test/EnterMemberCardTest.java
+++ SCS - Software - Test/src/org/lsmr/selfcheckout/software/test/EnterMemberCardTest.java
@@ -2,6 +2,7 @@ package org.lsmr.selfcheckout.software.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,6 +20,7 @@ import org.lsmr.selfcheckout.devices.TouchScreen;
 import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
 import org.lsmr.selfcheckout.devices.observers.TouchScreenObserver;
 import org.lsmr.selfcheckout.software.SelfCheckoutStationSoftware;
+import org.lsmr.selfcheckout.software.gui.EnterMemberCard;
 
 public class EnterMemberCardTest {
     private TouchScreen screen;
@@ -42,84 +44,44 @@ public class EnterMemberCardTest {
         
     }
 
+        
+    // Testing that "Enter member card number"
+    // got pressed.
     @Test
-    public void testBasic() {
-        screen.attach(new TouchScreenObserver() {
-            @Override
-            public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {}
-
-            @Override
-            public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {}
-        });
-        screen.detach(null);
-        screen.detachAll();
-        screen.disable();
-        screen.enable();
-    }
-
-    @Test
-    public void testFrameAutomatic() {
-        JFrame f = screen.getFrame();
-        JButton button = new JButton("foo");
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                found++;
-            }
-        });
-
-        f.add(button);
-
-        screen.setVisible(false);
-        button.doClick();
-
-        assertEquals(1, found);
-    }
-
-    // Note that this is not a proper automated test. An automated test does not
-    // force user interaction. Trust me: clicking repeatedly on buttons is tedious
-    // and error-prone. When you suddenly discover a bug on your hundredth attempt,
-    // what did you do? You stopped paying attention.  This is only a demo.
-    //
-    // Look at FrameDemo2 for a more detailed example of a standalone version.
-    @Test
-    public void testFrameManual() {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setPreferredSize(new Dimension(500, 500));
-                screen.setVisible(true);
-                JButton foo = new JButton("Press to Close");
-                foo.setPreferredSize(new Dimension(200, 100));
-                foo.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        found++;
-                        frame.dispose();
-                    }
-                });
-
-                frame.getContentPane().add(foo);
-                frame.pack();
-            }
-        });
-
-        // This loop is only needed to prevent the JUnit runner from closing the window
-        // before you have a chance to interact with it. If you look at FrameDemo2,
-        // which gets run as a standalone application, you will see that this is not
-        // necessary.
-        while(found < 1)
-            ;
-
-        assertEquals(1, found);
-    }
+    public void testMainMenuScreen() {
     
-    
-    @Test
-    public void enterMemberNumber() {
-    
+    	JFrame stationFrame = theSoftware.scs.screen.getFrame();
     	
+    	stationFrame.setSize(700,700);
+		stationFrame.getContentPane().setBackground(Color.LIGHT_GRAY);
+		stationFrame.setVisible(true);
+		// Gives us full control of where we want to place our GUI
+		// components.
+		stationFrame.setLayout(null);
+		
+		// Button for membershipnumber.
+		JButton enterMemberNumberButton = new JButton("Enter member card number");
+		enterMemberNumberButton.setSize(200,75);
+		stationFrame.add(enterMemberNumberButton);
+		enterMemberNumberButton.setLocation(450,550);
+		
+		enterMemberNumberButton.addActionListener(
+				new ActionListener()
+				{
+
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						// In essence the number of actions that occurred.
+						found++;
+						
+					}
+					
+				});
+    	
+		
+		enterMemberNumberButton.doClick();
+		assertEquals(1,found);
+		
     }
     
 }
